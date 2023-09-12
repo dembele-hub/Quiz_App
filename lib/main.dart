9ad19c6adc5986a541b2f094f7bd75902514c8f1
dembele_hub <chrisdembele5@gmail.com>
2023-09-12 14:24:10 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'Ui/Home.dart';
 
 void main() => runApp( MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(brightness: Brightness.dark,
+        primaryColor: Colors.lightGreenAccent,
+      ),
       home: const QuizApp(),
     ));
